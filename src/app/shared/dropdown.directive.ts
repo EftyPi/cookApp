864bc9d0855c2cd57175ebca838a361a4610523f
Eftychia Antoniou <eftychia.antoniou@ignitec.com>
2021-02-07 18:23:42 +0000

    added dropdown directive

---
+++ src/app/shared/dropdown.directive.ts
@@ -0,0 +1,21 @@
+import { Directive, ElementRef, HostBinding, HostListener } from '@angular/core';
+
+@Directive({
+  selector: '[appDropdown]'
+})
+export class DropdownDirective {
+  @HostBinding('class.open') isOpen = false;
+
+  // have to click on the element to close it
+  // @HostListener('click') toggleDown(){
+  //   this.isOpen = !this.isOpen;
+  // }
+  
+  // click anywhere to close it
+  @HostListener('document:click', ['$event']) toggleOpen(event: Event) {
+    this.isOpen = this.elRef.nativeElement.contains(event.target) ? !this.isOpen : false;
+  }
+
+  constructor(private elRef: ElementRef) { }
+
+}
